14e17daf7717258e1594988f8bfe9845daf6e6f8
Juho Suni <juho.suni@protonmail.com>
2020-07-04 15:25:03 +0300

    Part 4 - Blogs, initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const http = require('http')
+
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const Blog = require('./models/blog')
+
+app.use(cors())
+app.use(express.json())
+
+
+//-------------------------------
+//----------- GET ALL -----------
+//-------------------------------
+app.get('/api/blogs', (request, response) =>
+{
+    Blog
+        .find({})
+        .then(blogs =>
+        {
+            response.json(blogs)
+        })
+})
+
+
+//-------------------------------
+//---------- POST ONE -----------
+//-------------------------------
+app.post('/api/blogs', (request, response) =>
+{
+    const blog = new Blog(request.body)
+    blog
+        .save()
+        .then(result =>
+        {
+            response.status(201).json(result)
+        })
+})
+
+//-------------------------------
+//------------ PORT -------------
+//-------------------------------
+const PORT = process.env.PORT
+app.listen(PORT, () =>
+{
+    console.log(`Server running on port ${PORT}`)
+}) 
---
+++ models/blog.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose')
+// mongoose.set('useFindAndModify', false)
+// mongoose.set('useCreateIndex', true);
+// const password = process.argv[2]
+
+//-------------------------------
+//----------- SCHEMA ------------
+//-------------------------------
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+//const Blog = mongoose.model('Blog', blogSchema)
+module.exports = mongoose.model('Blog', blogSchema)
+
+//-------------------------------
+//----------- CONFIG ------------
+//-------------------------------
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------
+//---------- EXAMPLE ------------
+//-------------------------------
+// const mongoose = require('mongoose')
+// const uniqueValidator = require('mongoose-unique-validator')
+// mongoose.set('useFindAndModify', false)
+// mongoose.set('useCreateIndex', true);
+// const password = process.argv[2]
+
+// // const url =
+// //     `mongodb+srv://juho:${password}@hy-fullstack.gfdbm.mongodb.net/phonebook?retryWrites=true&w=majority`
+// const url = process.env.MONGODB_URI
+// console.log('connecting to', url)
+
+// personSchema.plugin(uniqueValidator)
+
+// personSchema.set('toJSON', {
+//     transform: (document, person) =>
+//     {
+//         person.id = person._id.toString()
+//         delete person._id
+//         delete person.__v
+//     }
+// })
+
+// //const Person = mongoose.model('Person', personSchema)
+
+// module.exports = mongoose.model('Person', personSchema)
